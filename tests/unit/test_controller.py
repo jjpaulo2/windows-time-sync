965b1aac3b46597ac4a9e65182bdd7e8740025e6
João Paulo Carvalho <jjpaulo2@protonmail.com>
2023-03-03 20:38:23 -0300

    feat(tests) - unit tests from windows integration (#2)

---
+++ tests/unit/test_controller.py
@@ -0,0 +1,26 @@
+from pytest import fixture, mark
+from pytest_mock import MockerFixture
+from unittest.mock import MagicMock, AsyncMock
+
+from windows_time_sync.controller import WindowsController
+
+
+@fixture
+def controller():
+    return WindowsController(
+        provider=AsyncMock()
+    )
+
+@fixture
+def set_system_time(mocker: MockerFixture):
+    return mocker.patch('windows_time_sync.controller.SetSystemTime')
+
+
+@mark.asyncio
+async def test_is_calling_windows_setsystemtime_method(
+    controller: WindowsController,
+    set_system_time: MagicMock
+):
+    await controller.set_time('')
+    set_system_time.assert_called_once()
+    
